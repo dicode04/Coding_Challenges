b0178459e51184f0d67536eed0f84f6b98d1e30f
dicode04 <divyarajrana04@gmail.com>
2023-05-01 17:01:48 -0500

    added C# coding challenge 2

---
+++ 050123/Collatz.cs
@@ -0,0 +1,23 @@
+﻿namespace Collatz;
+public class Program{
+    //collatz function to return a positive arbitrary integer if:
+    // n is even -> n/2
+    // n is odd -> 3n+1
+    static int Collatz(int n) {
+        int steps = 0;
+        while (n != 1) {           //while loop to iterate until n is equal to 1
+            if (n % 2 == 0) {
+                n /= 2;
+            } else {
+                n = 3 * n + 1; 
+            }
+            steps++;
+        }
+        return steps;
+    }
+    static void Main(string[] args){
+        int n = int.Parse(Console.ReadLine());
+        int steps = Collatz(n);
+        Console.WriteLine(steps);
+    }
+}
