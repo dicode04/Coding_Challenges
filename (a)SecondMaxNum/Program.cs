782e4d6478647ddfb2889e0c399265afee0b3b3d
dicode04 <divyarajrana04@gmail.com>
2023-05-26 00:24:24 -0500

    program for second max number

---
+++ (a)SecondMaxNum/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Program
+{
+    static int GetSecondMaximum(int a, int b, int c)
+    {
+        if ((a >= b && a <= c) || (a <= b && a >= c))
+            return a;
+        else if ((b >= a && b <= c) || (b <= a && b >= c))
+            return b;
+        else
+            return c;
+    }
+
+    static void Main(string[] args)
+    {
+        int N = int.Parse(Console.ReadLine()!);
+
+        for (int i = 0; i < N; i++)
+        {
+            string[]? input = Console.ReadLine()?.Split(' ');
+            if (input != null && input.Length == 3)
+            {
+                int num1 = int.Parse(input[0]);
+                int num2 = int.Parse(input[1]);
+                int num3 = int.Parse(input[2]);
+
+                int secondMax = GetSecondMaximum(num1, num2, num3);
+                Console.WriteLine(secondMax);
+            }
+        }
+    }
+}
