ff63d14cd01a097c7a1d63cad0c74900beb0bab6
dicode04 <divyarajrana04@gmail.com>
2023-05-26 00:25:18 -0500

    program for sum of all digit characters

---
+++ (b)SumofAllDigitChar/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+    static int SumOfDigits(string input)
+    {
+        int sum = 0;
+
+        foreach (char c in input)
+        {
+            if (Char.IsDigit(c))
+            {
+                int digit = c - '0';
+                sum += digit;
+            }
+        }
+
+        return sum;
+    }
+
+    static void Main(string[] args)
+    {
+        int T = int.Parse(Console.ReadLine()!);
+
+        for (int i = 0; i < T; i++)
+        {
+            string input = Console.ReadLine()!;
+            int sum = SumOfDigits(input);
+            Console.WriteLine(sum);
+        }
+    }
+}
